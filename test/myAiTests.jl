3f57cf1600a3199a8a5b0b2195a1c0d44a26527c
onButtonUp <jtferrier3@msn.com>
2019-11-10 18:08:31 -0800

    first aI test, adding file

--- test/myAiTests.jl
+++ test/myAiTests.jl
@@ -1,6 +1,8 @@
 using aI_Test
 using Test
 
+
+# OLD EXAMPLE TESTS vvv
 @testset "aI_Test.jl" begin
     # 2x + 3y
     @test aI_Test.my_f(2,1) == 7
@@ -11,3 +13,19 @@ end
 @testset "more testing" begin
     @test derivative_of_my_f(2,1) == 2
 end
+# OLD EXAMPLE TESTS ^^^
+
+
+# NEW aI TESTS vvv
+# @testset "some WAV testing" begin
+#     @test y, fs = wavread("example.wav")
+#;pwd()
+#file_dir = joinpath(dirname(@__FILE__), "test")
+
+@testset "real aI testing" begin
+    isfile(joinpath(dirname(@__FILE__),"example.wav"))
+#    isfile("example.wav")
+end
+
+# NEW aI TESTS ^^^
+;pwd()
