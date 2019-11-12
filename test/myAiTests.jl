259a6d20c1260571b32c9eb1712803ad1899d76e
onButtonUp <jtferrier3@msn.com>
2019-11-12 02:50:34 -0800

    added PGM WAV file and more tests

--- test/myAiTests.jl
+++ test/myAiTests.jl
@@ -26,15 +26,21 @@ end
 @testset "real aI testing" begin
     #hammerWav = joinpath(dirname(@__FILE__),"Hammerhead.wav")
     example02_ = joinpath(dirname(@__FILE__),"example02.wav")
+    pgm_ = joinpath(dirname(@__FILE__),"PGM102T1.wav")
 
     @test isfile(joinpath(dirname(@__FILE__),"example.wav"))
     @test isfile(joinpath(dirname(@__FILE__),"example02.wav"))
     @test isfile(joinpath(dirname(@__FILE__),"Hammerhead.wav"))
-    @test isfile(example02_)
+    @test isfile(pgm_)
     #y2, fs2 = WAV.wavread(hammerWav)
     y2, fs2 = WAV.wavread(example02_)
+    y_pgm, fs_pgm, nbits_pgm, opt_pgm = WAV.wavread(pgm_)
+
     #fs2 = 48000.0
-    @test fs2 == 16000.0 # versus 4800.0
+    @test fs2 == 16000.0 # versus 48000.0
+    @test fs_pgm == 44100
+    thisIt_pgm = WAV.getformat(opt_pgm)
+    @test thisIt_pgm.nchannels == 2
     #sqrt(sum(y2.^2.) / length(y2))
     #@test sqrt(sum(y2.^2.) / length(y2)) ≈ 0.16209680234518886
     @test sqrt(sum(y2.^2.) / length(y2)) ≈ 0.7070846851353825
@@ -43,10 +49,34 @@ end
 end
 
 #fs2
+
+### COMMENTED vv
 # example02_ = joinpath(dirname(@__FILE__),"example02.wav")
 # isfile(example02_)
-# y2, fs2 = WAV.wavread(example02_)
+# y2, fs2, nbits, opt = WAV.wavread(example02_)
 # fs2
+# nbits
+# opt
+# thisIt = WAV.getformat(opt)
+#
+# hammerhead_ = joinpath(dirname(@__FILE__),"Hammerhead.wav")
+# isfile(hammerhead_)
+# y_hammer, fs_hammer, nbits_hammer, opt_hammer = WAV.wavread(hammerhead_)
+# fs_hammer
+# nbits_hammer
+# opt_hammer
+# thisIt_hammer = WAV.getformat(opt_hammer)
+#
+#
+# pgm_ = joinpath(dirname(@__FILE__),"PGM102T1.wav")
+# isfile(pgm_)
+# y_pgm, fs_pgm, nbits_pgm, opt_pgm = WAV.wavread(pgm_)
+# fs_pgm
+# nbits_pgm
+# opt_pgm
+# thisIt_pgm = WAV.getformat(opt_pgm)
+### COMMENTED ^^
+
 # NEW aI TESTS ^^^
 #;pwd()
 #sqrt(sum(y2.^2.) / length(y2)) == 0.16209680234518886
